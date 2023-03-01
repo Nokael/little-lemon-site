650b72eca14607451dc8f8e86fa70d4a9e24a61f
nokael <nokael19@gmail.com>
2023-03-01 05:36:43 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Little Lemon</h1>
+      <p>hello customers...</p>
+      
+    </div>
+  )
+}
+
+export default App;
